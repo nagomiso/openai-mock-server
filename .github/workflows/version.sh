96ab197265f3db04f7f5610d4ededcc44780196f
nagomiso <daiseigi.giants+github@gmail.com>
2024-10-15 11:32:39 +0900

    Fix versioning method

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -8,15 +8,21 @@ tag_exists() {
   git rev-parse --verify "$1" > /dev/null 2>&1
 }
 
+openai_openapi_revision() {
+  git submodule status openai-openapi | awk '{print $1}' | cut -c 1-7
+}
+
 new_version_tag() {
-  prefix=$(new_version_tag_prefix)
-  if ! tag_exists "${prefix}"; then
-    echo "${prefix}"
+  prefix="$(new_version_tag_prefix)"
+  revision="$(openai_openapi_revision)"
+  tags="${prefix}-${revision}"
+  if ! tag_exists "${tags}"; then
+    echo "${tags}"
     return 0
   fi
   suffix=1
   while true; do
-    tag="${prefix}-${suffix}"
+    tag="${prefix}-${suffix}-${revision}"
     if ! tag_exists "${tag}"; then
       echo "${tag}"
       return 0
